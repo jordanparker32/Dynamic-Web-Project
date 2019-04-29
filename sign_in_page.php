90a6f73c67768ca8fc9ac54d3affe6042feb064d
Jordan Parker <31749489+jordanparker32@users.noreply.github.com>
2019-04-29 13:15:46 -0700

    Update sign_in_page.php

--- sign_in_page.php
+++ sign_in_page.php
@@ -1,20 +1,33 @@
-<?php include('server.php'); ?>
+
 <?php
-// define variables and set to empty values
-$username = $password = "";
+include('server.php');
+session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = test_input($_POST["username"]);
-  $password = test_input($_POST["password"]);
-    print($username);
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  // username and password sent from form 
+  
+  $myusername = mysqli_real_escape_string($db,$_POST['username']);
+  $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+  
+  $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+  $result = mysqli_query($db,$sql);
+  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  $active = $row['active'];
+  
+  $count = mysqli_num_rows($result);
+  
+  // If result matched $myusername and $mypassword, table row must be 1 row
+
+  if($count == 1) {
+     session_register("myusername");
+     $_SESSION['login_user'] = $myusername;
+     
+     header("location: home.html");
+  }else {
+     $error = "Your Login Name or Password is invalid";
+  }
 }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
 ?>
 
 <html>
