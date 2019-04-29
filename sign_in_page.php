9e0996700bc7c1d18aa2e0e15e7a3356058520b4
Jordan Parker <31749489+jordanparker32@users.noreply.github.com>
2019-04-29 16:40:04 -0700

    Update sign_in_page.php

--- sign_in_page.php
+++ sign_in_page.php
@@ -48,28 +48,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       <input type="submit" value="Submit" />
       <!--<input type="reset" /> -->
     </form>
-    <?php
-	if (isset($_POST['login']))
-		{
-			$username = mysqli_real_escape_string($con, $_POST['user']);
-			$password = mysqli_real_escape_string($con, $_POST['pass']);
-			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
-			$row		= mysqli_fetch_array($query);
-			$num_row 	= mysqli_num_rows($query);
-			
-			if ($num_row > 0) 
-				{			
-					$_SESSION['user_id']=$row['user_id'];
-					header('location:home.php');
-					
-				}
-			else
-				{
-					echo 'Invalid Username and Password Combination';
-				}
-		}
-  ?>
     <br />
     <br />
     <a href="sign_up_page.php">Click here to sign up!</a>
