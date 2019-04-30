41f43a45a93a69fee03e072259c0372855328bab
Jordan <jordanparker32@hotmail.com>
2019-04-29 18:55:17 -0700

    made and updloaded favicon

--- sign_in_page.php
+++ sign_in_page.php
@@ -4,24 +4,24 @@ include('server.php');
 session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-  // username and password sent from form 
-  
+  // username and password sent from form
+
   $myusername = mysqli_real_escape_string($db,$_POST['username']);
-  $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-  
+  $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
   $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
   $result = mysqli_query($db,$sql);
   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
   $active = $row['active'];
-  
+
   $count = mysqli_num_rows($result);
-  
+
   // If result matched $myusername and $mypassword, table row must be 1 row
 
   if($count == 1) {
      session_register("myusername");
      $_SESSION['login_user'] = $myusername;
-     
+
      header("location: home.html");
   }else {
      $error = "Your Login Name or Password is invalid";
@@ -33,6 +33,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
   <head>
     <title>Digital Address Book</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   </head>
   <link rel="stylesheet" type="text/css" href="homeStyleSheet.css" />
 
--- sign_up_page.php
+++ sign_up_page.php
@@ -2,12 +2,12 @@
     include('server.php');
 
     session_start();
-   
+
     if (isset($_POST['username']) && isset($_POST['password'])){
         $username = $_POST['username'];
 	      $email = $_POST['email'];
         $password = $_POST['password'];
- 
+
         $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
         $result = mysqli_query($connection, $query);
         if($result){
@@ -21,6 +21,7 @@
 <html>
   <head>
     <title>Sign-Up</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   </head>
   <link rel="stylesheet" type="text/css" href="homeStyleSheet.css" />
 
@@ -37,7 +38,7 @@
       <input type="text" name="password" /><br />
       <br>
       <input type="submit" value="Submit" />
-      <!--<input type="reset" /> --> 
+      <!--<input type="reset" /> -->
     </form>
   </body>
 </html>
