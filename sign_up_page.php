e41829696a8ab55272f63c6991ddc466c3fe3919
Jordan Parker <31749489+jordanparker32@users.noreply.github.com>
2019-05-01 01:07:51 -0700

    Update sign_up_page.php

--- sign_up_page.php
+++ sign_up_page.php
@@ -1,41 +1,52 @@
 <?php
+  
     include('server.php');
-
     session_start();
-
+    
+    if (!$db) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
     if (isset($_POST['username']) && isset($_POST['password'])){
-        $username = $_POST['username'];
-	      $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
-        $result = mysqli_query($connection, $query);
-        if($result){
-            $smsg = "User Created Successfully.";
-        }else{
-            $fmsg ="User Registration Failed";
-        }
+      $username=mysqli_real_escape_string($db,$_POST['username']);
+      $email=mysqli_real_escape_string($db,$_POST['email']);
+      $password=mysqli_real_escape_string($db,$_POST['password']);
+      $query = "INSERT INTO `users` (username, password, email) VALUES ('$username', '$email', '$password')";
+      //$query = "UPDATE `users` SET `username` = $username, `email` = $email, `password` = $password";
+      mysqli_query( $query, $db ) or trigger_error( mysql_error( $db ), E_USER_ERROR );
+      $result = mysqli_query($db, $query);
     }
-
+    mysqli_close($db);
 ?>
 <html>
   <head>
     <title>Sign-Up</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="homeStyleSheet.css" />
   </head>
-  <link rel="stylesheet" type="text/css" href="homeStyleSheet.css" />
 
+  <script>
+    function myFunction() {
+      var x = document.getElementById("myInput");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+</script>
   <body>
     <h1>Let's get you signed up!</h1>
     <br />
     <h3>Please enter required infomation below.</h3>
     <form action="choose_sites.html" method="post">
       Username:<br />
-      <input type="text" name="username"/><br />
+      <input type="text" name="username" required/><br />
       Email:<br />
-      <input type="text" name="email"/><br />
+      <input type="text" name="email" required/><br />
       Password:<br />
-      <input type="text" name="password" /><br />
+      <input type="password" name="password" value="passwrd" id="myInput"><br>
+      <input type="checkbox" onclick="myFunction()">Show Password
+      <br>
       <br>
       <input type="submit" value="Submit" />
       <!--<input type="reset" /> -->
